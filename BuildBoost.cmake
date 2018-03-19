159a46c50517a4fd63a0f67c468e3423f02b1072
Cameron Lowell Palmer <cameron@promon.no>
2018-03-19 14:14:21 +0100

    Added ability to override download

--- BuildBoost.cmake
+++ BuildBoost.cmake
@@ -7,8 +7,6 @@ set( BOOST_TARBALL boost_1_63_0.tar.bz2 )
 set( BOOST_URL "http://sourceforge.net/projects/boost/files/boost/1.63.0/${BOOST_TARBALL}" )
 set( BOOST_SHA1 "9f1dd4fa364a3e3156a77dc17aa562ef06404ff6" )
 
-#set( BOOST_GIT_REPO https://git.dev.promon.no/cameron/Boost.git )
-#set( BOOST_GIT_TAG 1.63.0 )
 set( BOOST_SHORT_VERSION 1.63.0 )
 
 set( BOOST_STANDARD_MODULES date_time filesystem system )
@@ -24,6 +22,10 @@ function( BuildBoost )
     set( TARGET_ARCHITECTURES ${BuildBoost_TARGET_ARCHITECTURES} )
     set( INSTALL_DIR ${BuildBoost_INSTALL_DIR} )
     set( BOOST_MODULES ${BuildBoost_MODULES} )
+
+    set( INSTALL_DIR_LIB ${INSTALL_DIR}/lib )
+    set( INSTALL_DIR_INCLUDE ${INSTALL_DIR}/include )
+
 
     if( BuildBoost_FRAMEWORK )
         set( GENERATE_FRAMEWORK true )
@@ -49,23 +51,29 @@ function( BuildBoost )
         list( APPEND BOOST_CXXFLAGS -fembed-bitcode )
     endif()
 
-    BOOST_${BOOST_MODULE}_STATIC_LIBS_TARGET_ARCHITECTURES
-    BOOST_${BOOST_MODULE}_SHARED_LIBS_TARGET_ARCHITECTURES
+    foreach( BOOST_MODULE ${BOOST_MODULES} )
+        set( BOOST_${BOOST_MODULE}_STATIC_LIBS_TARGET_ARCHITECTURES )
+        if( BUILD_SHARED )
+            set( BOOST_${BOOST_MODULE}_SHARED_LIBS_TARGET_ARCHITECTURES )
+        endif()
+    endforeach( BOOST_MODULE )
     foreach( TARGET_ARCHITECTURE ${TARGET_ARCHITECTURES} )
         set( BOOST_TARGET boost_${SDK}_${TARGET_ARCHITECTURE} )
         set( TARGET_INSTALL_DIR ${INSTALL_DIR}/${TARGET_ARCHITECTURE} )
         set( BOOST_STATIC_LIBS )
         set( BOOST_SHARED_LIBS )
 
+        ### Setup the name of the library
+        ### Setup the libraries that will be output (one per module) in this invocation of boost compile
+        ### Collect a list of the thin library modules to be lipo'd
         foreach( BOOST_MODULE ${BOOST_MODULES} )
-            #        set( BOOST_THIN_${BOOST_MODULE}_STATIC_LIBS_${SDK} )
-            ## Set up the name for the modules to be built libboost_filesystem.a for example
             set( BOOST_${BOOST_MODULE}_STATIC_LIB ${CMAKE_STATIC_LIBRARY_PREFIX}boost_${BOOST_MODULE}${CMAKE_STATIC_LIBRARY_SUFFIX} )
-            list( APPEND BOOST_STATIC_LIBS_${TARGET_ARCHITECTURE} ${TARGET_INSTALL_DIR}/${BOOST_${BOOST_MODULE}_STATIC_LIB} )
+            list( APPEND BOOST_STATIC_LIBS_${TARGET_ARCHITECTURE} ${TARGET_INSTALL_DIR}/lib/${BOOST_${BOOST_MODULE}_STATIC_LIB} )
+            list( APPEND BOOST_${BOOST_MODULE}_STATIC_LIBS_TARGET_ARCHITECTURES ${TARGET_INSTALL_DIR}/lib/${BOOST_${BOOST_MODULE}_STATIC_LIB} )
             if( BUILD_SHARED )
-                #            set( BOOST_THIN_${BOOST_MODULE}_SHARED_LIBS_${SDK} )
                 set( BOOST_${BOOST_MODULE}_SHARED_LIB ${CMAKE_SHARED_LIBRARY_PREFIX}boost_${BOOST_MODULE}${CMAKE_SHARED_LIBRARY_SUFFIX} )
-                list( APPEND BOOST_SHARED_LIBS_${TARGET_ARCHITECTURE} ${TARGET_INSTALL_DIR}/${BOOST_${BOOST_MODULE}_SHARED_LIB} )
+                list( APPEND BOOST_SHARED_LIBS_${TARGET_ARCHITECTURE} ${TARGET_INSTALL_DIR}/lib/${BOOST_${BOOST_MODULE}_SHARED_LIB} )
+                list( APPEND BOOST_${BOOST_MODULE}_SHARED_LIBS_TARGET_ARCHITECTURES ${TARGET_INSTALL_DIR}/lib/${BOOST_${BOOST_MODULE}_SHARED_LIB} )
             endif()
         endforeach( BOOST_MODULE )
 
@@ -92,16 +100,21 @@ function( BuildBoost )
         set_source_files_properties( ${CMAKE_RUN} PROPERTIES SYMBOLIC true )
 
         set( BOOST_TARBALL_PATH ${BOOST_SOURCE_DIR}/${BOOST_TARBALL} )
+
         set( TIME_CONDITION )
         if( EXISTS ${BOOST_TARBALL_PATH} )
             set( TIME_CONDITION --time-cond ${BOOST_TARBALL_PATH} )
+        endif()
+
+        if( OVERRIDE_TIMESTAMP_CHECK )
+            set( OVERRIDE_TIMESTAMP_TEST test -s ${BOOST_TARBALL_PATH} || )
         endif()
 
         ### Download the tarball if it has changed using the current copy for the timestamp
         add_custom_command( OUTPUT ${BOOST_TARBALL_PATH}
                 WORKING_DIRECTORY ${BOOST_SOURCE_DIR}
                 DEPENDS ${CMAKE_RUN}
-                COMMAND curl ${TIME_CONDITION} -o ${BOOST_TARBALL_PATH} --silent --location ${BOOST_URL}
+                COMMAND ${OVERRIDE_TIMESTAMP_TEST} curl ${TIME_CONDITION} -o ${BOOST_TARBALL_PATH} --silent --location ${BOOST_URL}
                 COMMENT "Downloading Boost source, if needed, for ${BOOST_TARGET}" )
 
         ### Untar and build
@@ -112,39 +125,30 @@ function( BuildBoost )
                 DEPENDS ${BOOST_TARBALL_PATH}
                 WORKING_DIRECTORY ${BOOST_SOURCE_DIR}
                 COMMAND tar xfz ${BOOST_TARBALL_PATH} --strip-components 1 -C ${BOOST_SOURCE_DIR}
-                COMMAND ${BOOST_SCRIPTS_DIR}/boost-compile.sh
+                COMMAND ${BOOST_SCRIPTS_DIR}/boost-build-ios.sh
                     ${SDK}
                     ${TARGET_ARCHITECTURE}
                     ${BOOST_SOURCE_DIR}
                     ${TARGET_INSTALL_DIR}
                     ${CXXFLAGS_FILE_PATH}
-                    ${MODULES_FILE_PATH} )
+                    ${MODULES_FILE_PATH}
+                COMMENT "Compiling Boost for ${TARGET_ARCHITECTURE}" )
     endforeach( TARGET_ARCHITECTURE )
 
-
-    set( INSTALL_DIR_LIB ${INSTALL_DIR}/lib )
-    set( INSTALL_DIR_INCLUDE ${INSTALL_DIR}/include )
+    set( BOOST_STATIC_LIBS )
+    if( BUILD_SHARED )
+        set( BOOST_SHARED_LIBS )
+    endif()
     foreach( BOOST_MODULE ${BOOST_MODULES} )
-        #        set( BOOST_THIN_${BOOST_MODULE}_STATIC_LIBS_${SDK} )
-        ## Set up the name for the modules to be built libboost_filesystem.a for example
-        set( BOOST_${BOOST_MODULE}_STATIC_LIB ${CMAKE_STATIC_LIBRARY_PREFIX}boost_${BOOST_MODULE}${CMAKE_STATIC_LIBRARY_SUFFIX} )
-        list( APPEND BOOST_STATIC_LIBS_${SDK} ${INSTALL_DIR_LIB}/${BOOST_${BOOST_MODULE}_STATIC_LIB} )
+        set( BOOST_${BOOST_MODULE}_STATIC_INSTALL_PATH ${INSTALL_DIR_LIB}/${BOOST_${BOOST_MODULE}_STATIC_LIB} )
+        Lipo( INPUTS ${BOOST_${BOOST_MODULE}_STATIC_LIBS_TARGET_ARCHITECTURES} OUTPUT ${BOOST_${BOOST_MODULE}_STATIC_INSTALL_PATH} )
+        list( APPEND BOOST_STATIC_LIBS ${BOOST_${BOOST_MODULE}_STATIC_INSTALL_PATH} )
         if( BUILD_SHARED )
-            #            set( BOOST_THIN_${BOOST_MODULE}_SHARED_LIBS_${SDK} )
-            set( BOOST_${BOOST_MODULE}_SHARED_LIB ${CMAKE_SHARED_LIBRARY_PREFIX}boost_${BOOST_MODULE}${CMAKE_SHARED_LIBRARY_SUFFIX} )
-            list( APPEND BOOST_SHARED_LIBS_${SDK} ${INSTALL_DIR_LIB}/${BOOST_${BOOST_MODULE}_SHARED_LIB} )
+            set( BOOST_${BOOST_MODULE}_SHARED_INSTALL_PATH ${INSTALL_DIR_LIB}/${BOOST_${BOOST_MODULE}_SHARED_LIB} )
+            Lipo( INPUTS ${BOOST_${BOOST_MODULE}_SHARED_LIBS_TARGET_ARCHITECTURES} OUTPUT ${BOOST_${BOOST_MODULE}_SHARED_INSTALL_PATH} )
+            list( APPEND BOOST_SHARED_LIBS ${BOOST_${BOOST_MODULE}_SHARED_INSTALL_PATH} )
         endif()
     endforeach( BOOST_MODULE )
-
-    foreach( BOOST_MODULE ${BOOST_MODULES} )
-        set( ${BOOST_MODULE}_INSTALL_PATH ${INSTALL_DIR_LIB}/${BOOST_MODULE_STATIC} )
-        Lipo( INPUTS ${${BOOST_MODULE}_STATIC_LIBS} OUTPUT ${${BOOST_MODULE}_INSTALL_PATH} )
-
-        if( BUILD_SHARED )
-            set( INSTALL_PATH_SSL_SHARED ${INSTALL_DIR_LIB}/${OPENSSL_LIBSSL_SHARED} )
-            Lipo( INPUTS ${OPENSSL_THIN_SSL_SHARED_LIBS_${SDK}} OUTPUT ${INSTALL_PATH_SSL_SHARED} )
-        endif()
-    endif()
 
     list( GET BOOST_INCLUDES_${SDK} 0 BOOST_INCLUDES_DIR )
     set( BOOST_INCLUDES_STAMP ${INSTALL_DIR}/boost_include.stamp)
@@ -156,6 +160,12 @@ function( BuildBoost )
             COMMAND touch ${BOOST_INCLUDES_STAMP}
             COMMENT "Copying Boost ${BOOST_INCLUDES_DIR} to ${INSTALL_DIR_INCLUDE}" )
 
-    set( BOOST_TARGET_${SDK} copy_boost_includes_${SDK} PARENT_SCOPE )
-
+    set( BOOST_INCLUDES_STAMP_${SDK} ${BOOST_INCLUDES_STAMP} PARENT_SCOPE )
+    set( BOOST_INCLUDES_DIR_${SDK} ${INSTALL_DIR_INCLUDE} PARENT_SCOPE )
+    foreach( BOOST_MODULE ${BOOST_MODULES} )
+        set( BOOST_${BOOST_MODULE}_STATIC_LIB_${SDK} ${BOOST_${BOOST_MODULE}_STATIC_INSTALL_PATH} PARENT_SCOPE )
+        if( BUILD_SHARED )
+            set( BOOST_${BOOST_MODULE}_SHARED_LIB_${SDK} ${BOOST_${BOOST_MODULE}_SHARED_INSTALL_PATH} PARENT_SCOPE )
+        endif()
+    endforeach( BOOST_MODULE )
 endfunction( BuildBoost )
